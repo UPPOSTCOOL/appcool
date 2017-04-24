49d1b04ae523f3dfb55fbf5ff2a50a428aa44403
sergio <dark07perez@gmail.com>
2017-04-23 23:18:10 -0500

    aregados archivos .sh y .pomp listo!

---
+++ push.sh
@@ -0,0 +1,12 @@
+echo Debio haber commitado primero!!!  comite, add .., commit -m
+echo ""
+echo ""
+git push origin master
+echo ""
+echo ""
+echo push Finalizado!!!
+echo "$$$$"
+echo "$$$$"
+echo "$$$$"
+echo Up Cool APP - Proyecto PDSW-2017-1
+echo Copyright © 2017 * teamAppCool*
