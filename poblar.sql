dd805702e095bcd1c17ef41b7e0c11b44661f7cb
kvncf <kevinju92@outlook.com>
2017-04-28 00:43:41 -0500

    agregando pre y corequisitos a poblar.sql

--- poblar.sql
+++ poblar.sql
@@ -11,17 +11,29 @@ insert into Asignatura
   (2,"Seguridad informática",1),
   (3,"datos",2),
   (4,"gestión",2);
-  
-  
+
 insert into Materia 
   (codigo,nombre,Asignatura_id,descripcion)
-  values---------
-  (1,"redes 1",1),
-  (2,"redes 2",1),
-  (3,"Seguridad 1",1),
-  (4,"Seguridad 2",1),
-  (5,"DNS",1),
-  (6,"datos gerenciales",2),
-  (7,"datos admon",2),
-  (8,"gestion correcta",2),
-  (9,"datos completos",2);
+  values
+  ("RED1","redes 1",1,"bases de redes"),
+  ("RED2","redes 2",1,"avanzado de redes"),
+  ("SEG1","Seguridad 1",2,"avanzado de seg"),
+  ("SEG2","Seguridad 2",2,"avanzado de seg"),
+  ("DNSS","DNS",2,"Domain Name Server"),
+  ("DAGE","datos gerenciales",3,"bases de gerencia"),
+  ("DAAD","datos admon",3,"avanzado de administración"),
+  ("GECO","gestion correcta",4,"correcta gestión de proyectos"),
+  ("DACO","datos completos",4,"avanzado datos informáticos");
+
+insert into Prerequisito
+  (Materia_id,Materia_id_pre)
+  values
+  ("RED2","RED1"),
+  ("SEG2","SEG1"),
+  ("DNSS","SEG1"),
+  ("GECO","DAGE");
+  
+insert into Corequisito
+  (Materia_id,Materia_id_co)
+  values
+  ("DAGE","SEG1");
