38db7f7c55117ec30a37694787b5d7af3c0aa089
kvncf <kevinju92@outlook.com>
2017-05-23 04:27:00 -0500

    agregar materia arreglos sin terminar

--- src/main/java/edu/eci/pdsw/uppostcool/managedbeans/RegistrarMateriasBean.java
+++ src/main/java/edu/eci/pdsw/uppostcool/managedbeans/RegistrarMateriasBean.java
@@ -13,6 +13,7 @@ import edu.eci.pdsw.uppostcool.services.ServiciosUPPOST;
 import edu.eci.pdsw.uppostcool.services.ServiciosUPPOSTFactory;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -27,11 +28,18 @@ import javax.faces.context.FacesContext;
 @SessionScoped
 public class RegistrarMateriasBean implements Serializable {
 
-    private List<Programa> programa = new ArrayList<>();
+    private List<Programa> programa = new ArrayList<>();//lista de programas
     private List<Asignatura> asignaturasProgramaSeleccionado = new ArrayList<>();
     private int id_asignaturaSeleccionada;
-    private Materia preRequisitos;
+    private Programa progParaAsigSeleccionado;
+    private int progParaMateriaSeleccionado;
+    private Materia preRequisitoSeleccionado;
+    private Materia coRequisitoSeleccionado;
     private Programa prog;
+    private List<Materia> MateriasParaPrerequisito;
+    private List<Materia> MateriasParaCorequisito;
+    private List<Materia> RequisitosAgregados;
+    private List<Materia> requisitos;
     
     ArrayList<Asignatura> acumulados= new ArrayList<>();
 
@@ -40,9 +48,25 @@ public class RegistrarMateriasBean implements Serializable {
     public List<Asignatura> getAsignaturasProgramaSeleccionado() {
         return asignaturasProgramaSeleccionado;
     }
-
-
+    
     public RegistrarMateriasBean() {
+    }
+
+    public List<Materia> getRequisitos() {
+        return requisitos;
+    }
+
+    public void setRequisitos(List<Materia> requisitos) {
+        this.requisitos = requisitos;
+    }
+    
+    public int getProgParaMateriaSeleccionado() {
+        return progParaMateriaSeleccionado;
+    }
+
+    public void setProgParaMateriaSeleccionado(int progParaMateriaSeleccionado) {
+        System.out.println("Guardando programa");
+        this.progParaMateriaSeleccionado = progParaMateriaSeleccionado;
     }
 
     public int getId_asignaturaSeleccionada() {
@@ -52,16 +76,56 @@ public class RegistrarMateriasBean implements Serializable {
     public void setId_asignaturaSeleccionada(int id_asignaturaSeleccionada) {
         this.id_asignaturaSeleccionada = id_asignaturaSeleccionada;
     }
+
+    public List<Materia> getRequisitosAgregados() throws ExcepcionServiciosUPPOST {
+        return RequisitosAgregados;
+    }
+
+    public void setRequisitosAgregados(List<Materia> RequisitosAgregados) {
+        this.RequisitosAgregados = RequisitosAgregados;
+    }
+
+    public Materia getPreRequisitoSeleccionado() {
+        return preRequisitoSeleccionado;
+    }
+
+    public void setPreRequisitoSeleccionado(Materia preRequisitoSeleccionado) {
+        this.preRequisitoSeleccionado = preRequisitoSeleccionado;
+    }
+
+    public Materia getCoRequisitoSeleccionado() {
+        return coRequisitoSeleccionado;
+    }
+
+    public void setCoRequisitoSeleccionado(Materia coRequisitoSeleccionado) {
+        this.coRequisitoSeleccionado = coRequisitoSeleccionado;
+    }
+
+    public List<Materia> getMateriasParaPrerequisito() throws ExcepcionServiciosUPPOST {
+        List<Materia> res=null;
+        if(progParaMateriaSeleccionado>0){
+            for (int i = 0; i <programa.size(); i++) {
+                if(programa.get(i).getId()==progParaMateriaSeleccionado)res=inp.consultarMateriasPrograma(progParaMateriaSeleccionado);
+            }
+        }
+        return res;
+    }
+
+    public void setMateriasParaPrerequisito(List<Materia> MateriasParaPrerequisito) {
+        this.MateriasParaPrerequisito = MateriasParaPrerequisito;
+    }
+
+    public List<Materia> getMateriasParaCorequisito() {
+        return MateriasParaCorequisito;
+    }
+
+    public void setMateriasParaCorequisito(List<Materia> MateriasParaCorequisito) {
+        this.MateriasParaCorequisito = MateriasParaCorequisito;
+    }
+    
     public List<Programa> obtenerProgramas() throws ExcepcionServiciosUPPOST {
         programa = inp.consultarProgramas();
         return programa;
-    }
-
-    public Materia requisitos() throws ExcepcionServiciosUPPOST {
-        String codigo = preRequisitos.getNombre();
-        preRequisitos = inp.consultarMateriaConRequisitos(codigo, 0);
-
-        return preRequisitos;
     }
 
     public void eliminarPrograma(int id_prog) {
@@ -79,6 +143,7 @@ public class RegistrarMateriasBean implements Serializable {
             if(programa.get(i).getId()==id_prog){
                 prog=programa.get(i);
                 asignaturasProgramaSeleccionado=programa.get(i).getAsignaturas();
+                progParaAsigSeleccionado=programa.get(i);
             }
         }
     }
@@ -86,34 +151,42 @@ public class RegistrarMateriasBean implements Serializable {
     public void guardarAsignaturaPrograma(){
         //obtenemos el id del programa para con él buscar cuál es 
         //la asignatura que se seleccionó
-        
+        Boolean yaIngresada=false;
         //en esta lista podemos guardar la asignatura que se seleccionó
-        for (int i = 0; i < programa.size(); i++) {
+        for (int i = 0; i < asignaturasProgramaSeleccionado.size(); i++) {
             if(asignaturasProgramaSeleccionado.get(i).getId()==id_asignaturaSeleccionada){
-                acumulados.add(asignaturasProgramaSeleccionado.get(i));
-                acumulados.get(i).getNombre();
+                asignaturasProgramaSeleccionado.get(i).setPostgrado(progParaAsigSeleccionado);
+                for (int a = 0; a < acumulados.size(); a++) {
+                    if(acumulados.get(a).getId()==id_asignaturaSeleccionada)yaIngresada=true;
+                }
+                if(!yaIngresada)acumulados.add(asignaturasProgramaSeleccionado.get(i));
             }
         }
     }
      
-
-    public ArrayList<String[]> getAcumulados() {
+    public List<String[]> getAcumulados() {
         
         //aca debemos ARMAR mediante acumulados los textos que se quieren mostrar en la tabla
         //recuerda que acumulados seraá una lista de ASIGNATURAS
-        
-        ArrayList<String[]> respuesta = new ArrayList<>();
-        for (int i=0;i<respuesta.size();i++){
-           
+        List<String[]> respuesta = new ArrayList<>();
+        String[] fila;
+        for (int i=0;i<acumulados.size();i++){
+            fila=new String[3];
+            fila[0]=Integer.toString(acumulados.get(i).getId());
+            fila[1]=acumulados.get(i).getPostgrado().getNombre();
+            fila[2]=acumulados.get(i).getNombre();
+            respuesta.add(fila);
         }
-        //= ejemplo:(id_asignatura,nombrePrograma,nombreAsignatura)
         return respuesta;
-        
     }
 
     public void setAcumulados(ArrayList<Asignatura> acumulados) {
         this.acumulados = acumulados;
     }
     
-   
+    public void eliminarAsignaturaPrograma(int id_asigEliminar){
+        for (int i=0;i<acumulados.size();i++){
+            if(acumulados.get(i).getId()==id_asigEliminar)acumulados.remove(i);
+        }
+    }
 }
