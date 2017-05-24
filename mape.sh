a7b9522ff35943a450ec04f9ee15c07bf45ecb2c
sergio <dark07perez@gmail.com>
2017-05-24 01:50:35 -0500

    OK: eliminando archivos de inicio de sesion por contener informacion sensible

--- mape.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.name "MariaPaulaP"
-git config --global user.email "maria.pinzon-c@mail.escuelaing.edu.co"
--- peli.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.name "Juan Felipe Ortiz Niño"
-git config --global user.email "jfelipeon@outlook.com"
